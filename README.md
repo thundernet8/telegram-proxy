8008652c42898e5f37dd53887b0cacf5d946d37d
wuxueqian <wuxueqian2010@hotmail.com>
2020-07-29 00:28:18 +0800

    format

--- README.md
+++ README.md
@@ -1,33 +1,31 @@
-# request-proxy
-转发telegram请求
+## telegram-proxy
+转发telegram请求，解决TG通知无法发送的问题
 
-
-# go
-
-## build
-
+示例
 ```bash
-go mod init main
-go mod download
-go build -o ./app main.go
+curl --location --request POST 'https://<your dmain>/<sender>:<telegram secret>/sendMessage' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+	"chat_id": <chat id>,
+	"text": "hello"
+}'
 ```
 
-# k8s
-
-## 构建 Image
+## 其他
+### 构建 Image
 
 ```bash
 docker build -t polywoo/telegram-proxy:v1 .
 ```
 
-## 上传 Image
+### 上传 Image
 
 ```bash
 docker login
 docker push polywoo/telegram-proxy:v1
 ```
 
-## 部署
+### K8S部署
 
 ```bash
 kubectl apply -f app.yaml
--- main.go
+++ main.go
@@ -8,53 +8,35 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	// "os"
-	// "strings"
 )
 
 type requestPayloadStruct struct {
 	Text string `json:"text"`
+	ChatId string `json:"chat_id"`
 }
 
-/*
-	Reverse Proxy Logic
-*/
-
-// Serve a reverse proxy for a given url
 func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
-	// parse the url
 	url, _ := url.Parse(target)
-
-	// create the reverse proxy
 	proxy := httputil.NewSingleHostReverseProxy(url)
-
-	// Update the headers to allow for SSL redirection
 	req.URL.Host = url.Host
 	req.URL.Scheme = url.Scheme
 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
 	req.Host = url.Host
-
-	// Note that ServeHttp is non blocking and uses a go routine under the hood
 	proxy.ServeHTTP(res, req)
 }
 
-// Get a json decoder for a given requests body
 func requestBodyDecoder(request *http.Request) *json.Decoder {
-	// Read body to buffer
 	body, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		log.Printf("Error reading body: %v", err)
 		panic(err)
 	}
 
-	// Because go lang is a pain in the ass if you read the body then any susequent calls
-	// are unable to read the body again....
 	request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 	return json.NewDecoder(ioutil.NopCloser(bytes.NewBuffer(body)))
 }
 
-// Parse the requests body
 func parseRequestBody(request *http.Request) requestPayloadStruct {
 	decoder := requestBodyDecoder(request)
 	var requestPayload requestPayloadStruct
@@ -67,7 +49,6 @@ func parseRequestBody(request *http.Request) requestPayloadStruct {
 	return requestPayload
 }
 
-// Given a request send it to the appropriate url
 func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
 	requestPayload := parseRequestBody(req)
 	url := "https://api.telegram.org"
@@ -79,7 +60,6 @@ func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	// start server
 	http.HandleFunc("/", handleRequestAndRedirect)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
